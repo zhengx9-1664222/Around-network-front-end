f9d03afd7176f61d2117f4fac92a1a73c7cde9d5
zhengx9-1664222 <35245400+zhengx9-1664222@users.noreply.github.com>
2020-02-05 14:46:04 -0800

    Add files via upload

---
+++ build/precache-manifest.7ae31da9e1b17cf232350950336f7ab9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbe1a75ace3d6f985747e4f2e82a47cf",
+    "url": "/zhengx9-1664222/Around-Social-Network/index.html"
+  },
+  {
+    "revision": "a555f76434db2ca6e474",
+    "url": "/zhengx9-1664222/Around-Social-Network/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "6b7b92ff48bfca9a17d3",
+    "url": "/zhengx9-1664222/Around-Social-Network/static/css/main.77190cb2.chunk.css"
+  },
+  {
+    "revision": "a555f76434db2ca6e474",
+    "url": "/zhengx9-1664222/Around-Social-Network/static/js/2.62eaef32.chunk.js"
+  },
+  {
+    "revision": "0d597d781630b7beb7660034904fdd6a",
+    "url": "/zhengx9-1664222/Around-Social-Network/static/js/2.62eaef32.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6b7b92ff48bfca9a17d3",
+    "url": "/zhengx9-1664222/Around-Social-Network/static/js/main.ff57c3b1.chunk.js"
+  },
+  {
+    "revision": "c0bddfb9768c49053d6a",
+    "url": "/zhengx9-1664222/Around-Social-Network/static/js/runtime-main.0a954267.js"
+  },
+  {
+    "revision": "13cf803d95d6519108a7125292fac131",
+    "url": "/zhengx9-1664222/Around-Social-Network/static/media/blue-marker.13cf803d.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/zhengx9-1664222/Around-Social-Network/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ build/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/zhengx9-1664222/Around-Social-Network/precache-manifest.7ae31da9e1b17cf232350950336f7ab9.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/zhengx9-1664222/Around-Social-Network/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
